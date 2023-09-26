f8a09930efb4809d6fb229953b29914520989abe
Eren <86615310+erent8@users.noreply.github.com>
2023-09-26 21:20:40 +0300

    Update main.py

--- music suggestion/main.py
+++ music suggestion/main.py
@@ -2,7 +2,8 @@ import requests
 import random
 
 # Spotify API Authorization token
-token = 'BQDk_rrqrpKNc0BIa63z5XTBfsRagBVIM8Dlw2ffoeMb2_fKD-NecCxzXsmOuquS8QTGtf7xOY1xqQGCOKZ38Xq5uQ0-WFS2GO75VjzI_6IiUFJDYtykzXBMMWdqgxTuXn4DJ4zcXPHbyqaOkb62BIexkjuzmUBY31Yq69sPFkW4AEfhPRi5azHFLCYDmwI_IHKyScfXdLMF1tqBM-cQ1ZTUo5r2mzoktLB7T5atDP1eFqYITCZSanLwJA0oxMWeN85PZ69ULbay9aUzJuyXv1z7KcMz'
+# API anahtarınızı buraya ekleyin
+token = 'YOUR_SPOTIFY_API_KEY'
 
 def fetch_web_api(endpoint, method, body=None):
     headers = {
@@ -17,7 +18,8 @@ def fetch_web_api(endpoint, method, body=None):
 
     return response.json()
 
-def get_top_tracks(limit=100):
+def get_top_tracks(limit=50):
+    # En iyi şarkıları almak için API endpoint'i
     endpoint = f'v1/me/top/tracks?time_range=short_term&limit={limit}'
     return fetch_web_api(endpoint, 'GET')
 
@@ -32,7 +34,7 @@ def get_random_tracks(limit=10):
     else:
         return ["Top tracks could not be fetched."]
 
-num_tracks = 50  # Almak istediğiniz şarkı sayısını burada ayarlayın
+num_tracks = 15  # Almak istediğiniz şarkı sayısını burada ayarlayın
 random_songs = get_random_tracks(num_tracks)
 print("Önerilen Şarkılar:")
 for i, song in enumerate(random_songs, start=1):
